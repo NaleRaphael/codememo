NaleRaphael/codememo

commit 9fce6f15392c6f0cdfe1b91e797355254dfccbb4

UPDATE: dump data if application crashes Major updates: - When there is an unexpected error occurs and makes application   crash, data in opening `CodeNodeViewer` will be dumped into a   folder locating under ".codememo/dump_[time]/".
Minor updates/revisions:
- Shows name of opening project at the title of `CodeNodeViewer`.

codememo/components.py
@@ -96,7 +96,7 @@ class MenuBar(ImguiComponent):
                 except Exception as ex:
                     GlobalState().push_error(ex)
                 else:
-                    viewer = CodeNodeViewer(self.app, node_collection)
+                    viewer = CodeNodeViewer(self.app, node_collection, fn_src=fn)
                     self.app.add_component(viewer)
 
             self.file_dialog = OpenFileDialog(self.app, open_project)
@@ -681,6 +681,8 @@ class CodeNodeViewer(ImguiComponent):
         """
         self.app = app
         self.fn_src = fn_src
+        fn = 'untitled' if fn_src is None else Path(fn_src).with_suffix('').name
+        self.window_name = f'CodeNode Viewer: {fn}'
         self.node_collection = node_collection
         self.node_components = []
         self.links = []
@@ -711,6 +713,10 @@ class CodeNodeViewer(ImguiComponent):
 
     def save_data(self, fn):
         self.node_collection.save(fn)
+
+        # Update window name
+        self.fn_src = fn
+        self.window_name = f"CodeNode Viewer: {Path(fn).with_suffix('').name}"
 
     def init_nodes_and_links(self):
         trees, orphans = self.node_collection.resolve_tree()
@@ -1042,7 +1048,9 @@ class CodeNodeViewer(ImguiComponent):
         self.app = None
 
     def render(self):
-        _, self.opened = imgui.begin('CodeNodeViewer', closable=True, flags=imgui.WINDOW_MENU_BAR)
+        _, self.opened = imgui.begin(
+            self.window_name, closable=True, flags=imgui.WINDOW_MENU_BAR
+        )
         if not self.opened:
             imgui.end()
             self.close()

codememo/app.py
@@ -41,11 +41,14 @@ class Application(object):
     def init_draw_process(self):
         def update(dt):
             imgui.new_frame()
-            for component in self.imgui_components:
-                while self._internal_state.error_occured:
-                    error = self._internal_state.pop_error()
-                    self.add_component(ErrorMessageModal(self, str(error)))
-                component.render()
+            try:
+                for component in self.imgui_components:
+                    while self._internal_state.error_occured:
+                        error = self._internal_state.pop_error()
+                        self.add_component(ErrorMessageModal(self, str(error)))
+                    component.render()
+            except Exception as ex:
+                self.dump_data(ex)
 
         @self.window.event
         def on_draw():
@@ -76,6 +79,31 @@ class Application(object):
     def remove_component(self, component):
         idx = self.imgui_components.index(component)
         self._removed_components.append(self.imgui_components.pop(idx))
+
+    def dump_data(self, error):
+        """Dump data if unexpected error occured."""
+        from pathlib import Path
+        from datetime import datetime
+        import traceback
+
+        crash_time = datetime.now().strftime('%Y%m%d-%H%M%S')
+        dir_dump = Path(self.config.dir_config, f'dump_{crash_time}')
+        dir_dump.mkdir(parents=True, exist_ok=True)
+
+        serial_num = 0
+        for component in self.imgui_components:
+            if isinstance(component, CodeNodeViewer):
+                if component.fn_src is None:
+                    fn = f'untitled_{serial_num}.json'
+                    serial_num += 1
+                else:
+                    fn = Path(component.fn_src).with_suffix('.json').name
+                component.save_data(str(Path(dir_dump, fn)))
+
+        fn_log = str(Path(dir_dump, 'traceback.txt'))
+        with open(fn_log, 'w') as f:
+            traceback.print_tb(error.__traceback__, limit=None, file=f)
+            f.write(str(error))
 
     def finalize(self):
         while self._removed_components:

codememo/config.py
@@ -62,6 +62,8 @@ class AppConfig(ConfigBase):
     def __init__(self, **kwargs):
         super(AppConfig, self).__init__()
         kwargs = {} if kwargs is None else kwargs
+        self.dir_config = AppDefaults.dir_config
+        self.fn_config = AppDefaults.fn_config
         self.display = DisplayConfig(
             **kwargs.pop(DisplayConfig.name, {})
         )

commit 59f3697037eaec55924978376e3a42a6d57e0bdf

BUG: "add_reference" event will be cancelled immediately when mouse cursor moves
This bug is introduced in commit 9fce6f1.

Since the facility of "add_reference" for nodes was implemented in
commit 10a794a, it relied on `imgui.set_window_focus_labeled()`
to bring user back to `CodeNodeViewer` and make them able to select
a node. And the event will be cancelled once user clicks elsewhere
(that is, event will be cancelled when `CodeNodeViewer` lost focus).

However, in commit 9fce6f1, window name of a `CodeNodeViewer` going
to be rendered is changed to the value `self.window_name` instead
of the constant string "CodeNodeViewer". But we forgot to change
the argument for `imgui.set_window_focus_labeled()`. And that's
why `CodeNodeViewer` won't be focused and resulted the issue
mentioned as the title of this commit.

codememo/components.py
@@ -709,8 +709,13 @@ class CodeNodeViewer(ImguiComponent):
         """
         self.app = app
         self.fn_src = fn_src
+
+        # NOTE: We should avoid duplicate of window name (identifier) because
+        # it relates to the focus mechanism. See also the implementation of
+        # `CodeNodeViewer.handle_event__add_reference()`
         fn = 'untitled' if fn_src is None else Path(fn_src).with_suffix('').name
         self.window_name = f'CodeNode Viewer: {fn}'
+
         self.node_collection = node_collection
         self.node_components = []
         self.links = []
@@ -928,10 +933,12 @@ class CodeNodeViewer(ImguiComponent):
         else:
             self.reset_panning_delta()
 
+    # TODO: Replace name-based handler resolution with a decorator to mark
+    # this method as a valid event handler.
     # event handler for "add_reference"
     def handle_event__add_reference(self, event):
         self.state_cache['event__add_reference'] = event
-        imgui.set_window_focus_labeled('CodeNodeViewer')
+        imgui.set_window_focus_labeled(self.window_name)
 
     def handle_event__create_node(self, event):
         node = event.get('node')